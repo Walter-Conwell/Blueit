be9ae52d23b9e28258eb5bc8a81646faa4ee0c54
William Brown <willis291@gmail.com>
2023-06-29 14:18:25 -0600

    fixing user profile model

--- models/UserProfile.js
+++ models/UserProfile.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+const { TEXT } = require("sequelize");
 
 class UserProfile extends Model {}
 
@@ -11,13 +12,28 @@ UserProfile.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    username: {},
+    username: {
+      type: varchar(255),
+      allowNull: false,
+      unique: true,
+    },
+    name: {
+      type: varchar(255),
+      allowNull: true,
+    },
+
     user_cred_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
         model: "user_credentials",
         key: "id",
+      },
+      photo_file: {
+        type: varchar(255),
+      },
+      bio: {
+        type: DataTypes.TEXT,
       },
     },
   },
