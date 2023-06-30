9e9212dd1b290895be753c60e0196d68171aa1d7
Paulsig007 <131915869+Paulsig007@users.noreply.github.com>
2023-06-30 12:45:13 -0600

    add blogpost model, update server.js

---
+++ models/BlogPost.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connections");
+
+class BlogPost extends Model {}
+
+BlogPost.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    post_date: {
+      type: DataTypes.DATE,
+    },
+    user_profile_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user_profile",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    updatedAt: "updateTimestamp",
+    freezeTableName: true,
+    underscored: true,
+    modelName: "blog_post",
+  }
+);
+
+module.exports = BlogPost;
