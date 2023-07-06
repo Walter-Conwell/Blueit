02966a60c8ecf4ae62264b27ba9a21ba2c39a378
William Brown <willis291@gmail.com>
2023-07-05 21:23:17 -0600

    Got blog post from website functioning

--- public/js/blogWrite.js
+++ public/js/blogWrite.js
@@ -27,7 +27,9 @@ var quill = new Quill("#editor", {
   theme: "snow",
 });
 
+//This is the listener for the save button
 function getBlogContent() {
+  // console.log(req.session.user_id);
   var blogTitle = document.getElementById("blogTitle").value;
   var blogTopic = document.getElementById("blogTopic").value;
   var blogContent = quill.root.innerHTML;
@@ -38,8 +40,25 @@ function getBlogContent() {
     content: blogContent,
   };
 
+  saveBlogContent(blogPost);
+
   // do something with blog post data
   console.log(blogPost);
+}
+
+//this actually saves the content to SQL database
+async function saveBlogContent(blogPost) {
+  const response = await fetch("/api/blogposts", {
+    method: "POST",
+    body: JSON.stringify({
+      post_title: blogPost.title,
+      post_text: blogPost.content,
+      // user_id: req.session.user_id, //This is just a placeholder for now
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
 
 var saveBtn = document.getElementById("saveBlogBtn");
